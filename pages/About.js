44b03acd97dc5b38573dae882abfc65bc3d632e9
riad <riad@gmail.com>
2022-07-03 11:48:16 +0100

    i will rabse

--- pages/About.js
+++ pages/About.js
@@ -1,6 +1,6 @@
 import UserList from '../components/UserList';
 const About = () => {
-  return <div>I am riad from github 2 3</div>;
+  return <div>I am riad from github 2 3 4 from local repo</div>;
 };
 
 export default About;
