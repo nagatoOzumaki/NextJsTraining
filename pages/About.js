ff4a7e6957341aac7277ab3161ee66de1bb22a89
nagatoOzumaki <99969573+nagatoOzumaki@users.noreply.github.com>
2022-07-03 11:25:52 +0100

    Update About.js

--- pages/About.js
+++ pages/About.js
@@ -1,6 +1,6 @@
 import UserList from '../components/UserList';
 const About = () => {
-  return <div>I am riad from github 2</div>;
+  return <div>I am riad from github 2 3</div>;
 };
 
 export default About;
